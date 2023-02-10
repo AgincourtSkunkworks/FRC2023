7f8f9a02c7b1514938f6d0328a665358d8e52db3
Theodore Hua <theodorehuadev@gmail.com>
2023-02-10 18:23:04 -0500

    Add delay after driving, correct turn radius, remove redundant code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,6 +50,7 @@ public class Robot extends TimedRobot {
     // Configuration Variables
     final int startPosOverride = -2; // -2 = None; -1 = Left; 0 = Center; 1 = Right
     final int initialState = 0; // Initial state when autonomous is enabled (used for debugging usually)
+    final int turnRadius = 78; // Amount to turn when trying to do a 90 degree turn. Generally to account for drift.
     final double teleopMoveScale = 0.7; // Percent to scale the controller input by when moving (forward or backward)
     final double teleopTurnScale = 0.5; // Percent to scale the controller input by when turning (controllers aren't in same direction [0 is considered no direction])
     final double leftMotorSpeedOffset = 0.08; // Percent offset (0-1) for left motor speed (to ensure that it can drive straight)
@@ -62,6 +63,7 @@ public class Robot extends TimedRobot {
     final double autonomousMoveSpeed = 0.4; // Speed to move at normally while in automous
     final double autonomousTurnSpeed = 0.3; // Speed to turn at while in autonomous mode
     final double autonomousDockSpeed = 0.2; // Speed to move forward while attempting to dock
+    final long autonomousPostDriveDelay = 500; // Delay (in milliseconds) after driving to the center, so we don't carry energy into the turn and overshoot it.
     final long autonomousTurnCheckInterval = 25; // Interval to check gyro while turning
     final long autonomousFloorCheckInterval = 100; // Interval to check gryo at to determine if we're at the docking station (in milliseconds)
     final long autonomousDockCheckInterval = 100; // Interval to check gyro at while attempting to dock (in milliseconds)
@@ -98,16 +100,6 @@ public class Robot extends TimedRobot {
     private void setRightMotorSpeed(double speed) {
         for (TalonSRX motor : rightMotors)
             motor.set(ControlMode.PercentOutput, speed + speed * rightMotorSpeedOffset);
-    }
-
-    /**
-     * Set All Motor Speeds
-     * 
-     * @param speed Percent of maximum motor speed (1 being max)
-     */
-    private void setMotorSpeed(double speed) {
-        setLeftMotorSpeed(speed);
-        setRightMotorSpeed(speed);
     }
 
     /**
@@ -177,10 +169,11 @@ public class Robot extends TimedRobot {
          * 1 - Post-Initialization (Unmoved)
          * 2 - Turning Stage 1 (turn toward center, perpendicular to docking station)
          * 3 - Turning Stage 2 (driving to center)
-         * 4 - Turning Stage 3 (turn toward docking station)
-         * 5 - Moving to Charging
-         * 6 - Charging Station Docking
-         * 7 - Done Docking (in theory)
+         * 4 - Turning Stage 3 (delay after driving to center)
+         * 5 - Turning Stage 4 (turn toward docking station)
+         * 6 - Moving to Charging
+         * 7 - Charging Station Docking
+         * 8 - Done Docking (in theory)
          * -1 - Error State
          */
         state = initialState;
@@ -214,11 +207,12 @@ public class Robot extends TimedRobot {
             else state = (startPos != 0) ? 2 : 5; // Reminder: Only set state when startPos has been successfully determined
         } else if (state == 2) { // Turning Stage 1 (if needed) -- turn 90 to face center (perpendicular to docking)
             if (curTime - lastRunTime < autonomousTurnCheckInterval) {}
-            else if (turnRobot(autonomousTurnSpeed, (startPos == -1) ? 90 : -90, !waiting)) {
+            else if (turnRobot(autonomousTurnSpeed, (startPos == -1) ? turnRadius : -turnRadius, !waiting)) {
                 waiting = false;
                 lastRunTime = 0;
                 state = 3; // Turning stage 2
             } else {
+                setMotorSpeedCorrected(0);
                 waiting = true;
                 lastRunTime = curTime;   
             }
@@ -234,17 +228,28 @@ public class Robot extends TimedRobot {
                 waiting = false;
                 state = 4;
             }
-        } else if (state == 4) { // Turning Stage 3 (if needed) -- turn 90 to face docking station
+        } else if (state == 4) { // Turning Stage 3 (if needed) -- wait a while before turning to face docking station
+            if (!waiting) {
+                lastRunTime = curTime;
+                waiting = true;
+            }
+            if (curTime - lastRunTime >= autonomousPostDriveDelay) {
+                lastRunTime = 0;
+                waiting = false;
+                state = 5; // Move to docking station
+            }
+        } else if (state == 5) { // Turning Stage 4 (if needed) -- turn 90 to face docking station
             if (curTime - lastRunTime < autonomousTurnCheckInterval) {}
-            else if (turnRobot(autonomousTurnSpeed, (startPos == -1) ? -90 : 90, !waiting)) {
+            else if (turnRobot(autonomousTurnSpeed, (startPos == -1) ? -turnRadius : turnRadius, !waiting)) {
                 waiting = false;
                 lastRunTime = 0;
-                state = 5; // Move to docking station
+                state = 6; // Move to docking station
             } else {
+                setMotorSpeedCorrected(0);
                 waiting = true;
                 lastRunTime = curTime;
             }
-        } else if (state == 5) { // Moving Forward to Charging Station
+        } else if (state == 6) { // Moving Forward to Charging Station
             if (!waiting) {
                 setMotorSpeedCorrected(autonomousMoveSpeed);
                 waiting = true;
@@ -255,12 +260,12 @@ public class Robot extends TimedRobot {
                 if (onFloorMin > pitchDegrees || pitchDegrees > onFloorMax) {
                     if (debugMode) System.out.printf("Pitch OUT of floor range (%f out of %f-%f)%n", pitchDegrees, onFloorMin, onFloorMax);
                     setMotorSpeedCorrected(0);
-                    state = 6; // Start docking
+                    state = 7; // Start docking
                     waiting = false;
                     lastRunTime = 0;
                 } else if (debugMode) System.out.printf("Pitch IN floor range (%f <= %f <= %f)%n", onFloorMin, pitchDegrees, onFloorMax);
             }
-        } else if (state == 6) { // Dock with Charging Station
+        } else if (state == 7) { // Dock with Charging Station
             if (!waiting) {
                 setMotorSpeedCorrected(autonomousDockSpeed);
                 waiting = true;
@@ -271,7 +276,7 @@ public class Robot extends TimedRobot {
                 if (dockedMin <= pitchDegrees && pitchDegrees <= dockedMax) {
                     if (debugMode) System.out.printf("Dock IN range (%f <= %f <= %f)%n", dockedMin, pitchDegrees, dockedMax);
                     setMotorSpeedCorrected(0);
-                    state = 7; // Finished docking
+                    state = 8; // Finished docking
                     waiting = false;
                     lastRunTime = 0;
                 } else if (debugMode) System.out.printf("Dock OUT of range (%f out of %f-%f)%n", pitchDegrees, dockedMin, dockedMax);
